85892031d796d622481dc87cf6b860ab81583ad8
webhasibulislam <44059641+webhasibulislam@users.noreply.github.com>
2019-09-15 13:22:19 -0700

    All file and folders of blog website
    a dynamic blog website

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "id8269539_hasibulblog");
+define("DB_PASS", "hasibulislam420");
+define("DB_NAME", "id8269539_db_blog");
+define("TITLE", "w3blog");
+define("KEYWORDS", "PHP, java, html, css, web design, web development, sql, JavaScript, php oop");
+?>
